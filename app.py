30a8b6928c4e96b14c7ba163e66511df0ca8e3c7
Gabriel <gabrielpaduat@gmail.com>
2026-06-23 17:12:20 -0300

    refatorando codigo inicial

--- app.py
+++ app.py
@@ -1,27 +1,38 @@
 import os
 
-print("Sabor Express\n")
-
-print("1. Cadastrar Restaurante")
-print("2. Listar Restaurante")
-print("3. Ativar Restaurante")
-print("4. Sair\n")
+def exibir_nome_do_programa():
+    print("Sabor Express\n")
 
 
-opcao_escolhida = int(input("Escolha uma opcão: "))
+def exibir_opcoes():
+    print("1. Cadastrar Restaurante")
+    print("2. Listar Restaurantes")
+    print("3. Ativar Restaurante")
+    print("4. Sair\n")
 
 def finalizar_app():
     os.system("cls")
-    print("Encerrando o Programa\n")
+    print("Finalizando o APP!\n")
 
 
+def escolher_opcao():
+    opcao_escolhida = int(input("Escolha uma opcão: "))
+    match opcao_escolhida:
+        case 1:
+            print("Cadastrar restaurante")    
+        case 2:
+            print("Listar restaurantes")    
+        case 3:
+            print("Ativar restaurante")   
+        case _:
+           finalizar_app() 
 
 
-if opcao_escolhida == 1 :
-    print("Cadastrar restaurante")    
-elif opcao_escolhida == 2 :
-    print("Listar restaurantes")    
-elif opcao_escolhida == 3 :
-    print("Ativar restaurante")
-else:
-    finalizar_app()
+def main():
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcao()
+
+
+if __name__ == "__main__":
+    main()
